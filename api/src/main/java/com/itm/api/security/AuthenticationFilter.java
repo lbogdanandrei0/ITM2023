3e8b6756c209dd1a897d6eeb1479f07df3757349
lbogdanandrei0 <lungubogdanandrei0@gmail.com>
2023-04-01 16:40:55 +0300

    small fix

--- api/src/main/java/com/itm/api/security/AuthenticationFilter.java
+++ api/src/main/java/com/itm/api/security/AuthenticationFilter.java
@@ -118,11 +118,12 @@ public class AuthenticationFilter extends OncePerRequestFilter {
         }
         auth = auth.replaceAll("Basic ", "");
         String[] parts = new String(Base64.getDecoder().decode(auth.getBytes(StandardCharsets.UTF_8)), StandardCharsets.UTF_8).split(":");
-        if (parts.length != 2) {
-            return loginDTO;
+        if (parts.length >= 1) {
+            loginDTO.setUsername(parts[0]);
         }
-        loginDTO.setUsername(parts[0]);
-        loginDTO.setPassword(parts[1]);
+        if (parts.length >= 2) {
+            loginDTO.setPassword(parts[1]);
+        }
         return loginDTO;
     }
 }
