58f73d900358f5ee95b7c37a77833105ddee9a88
lbogdanandrei0 <lungubogdanandrei0@gmail.com>
2023-04-01 19:56:22 +0300

    update interface

--- Interfata/main.py
+++ Interfata/main.py
@@ -1,5 +1,6 @@
 import json
 from base64 import b64encode
+import datetime
 import requests
 from flask import Flask, render_template, request, redirect, make_response
 
@@ -58,13 +59,39 @@ def home_fct():
 
 @app.route('/view', methods=['GET', 'POST'])
 def view_fct():
-    return render_template('view.html')
+    global data
+    username = data['username']
+    headers = {'Authorization': basic_auth(username, "")}
+    response = requests.get("http://localhost:9091/api/break-request", headers=headers)
+    breakRequestData = json.loads(response.content)
+    return render_template('view.html', data=breakRequestData)
 
 
 @app.route('/send', methods=['GET', 'POST'])
 def send_fct():
-    return render_template('send.html')
+    global data
+    username = data['username']
+    headers = {'Authorization': basic_auth(username, "")}
+    response = requests.get(f"http://localhost:9091/api/timeline/range?startDate=2023-04-01T00:00:00Z&endDate=2023-04-01T23:59:59Z", headers=headers)
+    timelines = json.loads(response.content)
+    return render_template('send.html', timelines=timelines)
 
+
+@app.route('/sendInvite', methods=['GET', 'POST'])
+def send_invite():
+    global data
+    username = data['username']
+    headers = {'Authorization': basic_auth(username, "")}
+    place = request.form['placeInput']
+    comment = request.form['commentInput']
+    page_ids = request.form.getlist("participant")
+    json_data = {
+        "place": place,
+        "comment": comment,
+        "timelineUuids": page_ids
+    }
+    response = requests.post("http://localhost:9091/api/break-request", json=json_data, headers=headers)
+    return redirect('/send')
 
 @app.route('/search', methods=['GET', 'POST'])
 def search_fct():
@@ -90,6 +117,10 @@ def search_fct():
 @app.route('/profile')
 def profile_fct():
     global data
+    username = data['username']
+    headers = {'Authorization': basic_auth(username, "")}
+    response = requests.get(f"http://localhost:9091/api/user/timeline", headers=headers)
+    timelines = json.loads(response.content)
     e1 = ["08:00", "09:00", "Busy"]
     e2 = ["09:00", "10:00", "Busy"]
     e3 = ["10:00", "11:00", "Busy"]
@@ -99,7 +130,7 @@ def profile_fct():
     e7 = ["14:00", "15:00", "Busy"]
     e8 = ["15:00", "16:00", "Busy"]
     e9 = ["16:00", "17:00", "Busy"]
-    e10 = ["17:00", "18:00", "Free"]
+    e10 = ["17:00", "18:00", "Busy"]
     elements = [e1, e2, e3, e4, e5, e6, e7, e8, e9, e10]
     return render_template('profile.html', elements=elements, info=data)
 
